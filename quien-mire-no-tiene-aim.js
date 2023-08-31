7ec527778580651f1479777202ebe9e02b63511e
Lafsitoo <fretteleandro@live.com>
2023-08-30 21:40:04 -0300

    changes

--- quien-mire-no-tiene-aim.js
+++ quien-mire-no-tiene-aim.js
@@ -1,3 +1,20 @@
-let user = "Lafsitoo"
+const precioEntrada = 4000;
+const descuentoMenorEdad = 40;
+const descuentoAdolescente = 20;
+const descuentoJubilado = 70;
+let totalAbonar = 0;
 
-console.log(`Bienvenido ${firstName} ${lastName}`)
+const leer = (edad) => {
+  if (edad < 16) {
+    totalAbonar = precioEntrada - (precioEntrada * descuentoMenorEdad) / 100;
+    return `El precio de la entrada es de $${totalAbonar}`;
+  } else if (edad >= 6 && edad <= 18) {
+    totalAbonar = precioEntrada - (precioEntrada * descuentoAdolescente) / 100;
+    return `El precio de la entrada es de $${totalAbonar}`;
+  } else if (edad > 65) {
+    totalAbonar = precioEntrada - (precioEntrada * descuentoJubilado) / 100;
+    return `El precio de la entrada es de $${totalAbonar}`;
+  }
+};
+
+console.log(leer(30));
