cb0f16094c6dbbfc0e995cae840cd354e1b9377e
Lafsitoo <fretteleandro@live.com>
2023-10-18 16:11:47 -0300

    add all products local storage

--- js/products.js
+++ js/products.js
@@ -35,6 +35,42 @@ const listProducts = [
     price: "$1500",
     dateEntry: "2023-06-09",
   },
+  {
+    id: "cfa5fc00-3624-48a1-8f64-c09de17a0adb",
+    name: "Matcha green",
+    image: "/assets/images/matcha-green.jpg",
+    description:
+      "Té verde en polvo con un sabor intenso y aromático, rico en antioxidantes y vitaminas.",
+    price: "$1400",
+    dateEntry: "2023-06-01",
+  },
+  {
+    id: "95815fb4-ab09-4c47-bed7-e2c0fa13fa7f",
+    name: "Latte",
+    image: "/assets/images/latte.jpg",
+    description:
+      "Bebida de café con leche vaporizada y espuma de leche, con un sabor suave y equilibrado.",
+    price: "$1500",
+    dateEntry: "2023-05-11",
+  },
+  {
+    id: "0b4d800d-6299-49a6-a432-1736f0773f5a",
+    name: "Capuchino",
+    image: "/assets/images/capuchino.jpg",
+    description:
+      "Bebida de café con espresso, leche vaporizada y espuma de leche, con un sabor rico y cremoso.",
+    price: "$1450",
+    dateEntry: "2023-04-21",
+  },
+  {
+    id: "048e1efd-a1b1-4119-b353-aa49eb2ebb0f",
+    name: "Expreso",
+    image: "/assets/images/expreso.jpg",
+    description:
+      "Bebida de café concentrada con un sabor intenso y aromático, ideal para despertar o recargar energías.",
+    price: "$1300",
+    dateEntry: "2023-04-09",
+  },
 ];
 
 // Convierte la lista de productos en una cadena JSON
